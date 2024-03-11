54ae55349eaa8370c610db000f9d4164cc49f830
halik <halik@spsejecna.cz>
2024-03-11 09:52:01 +0100

    added new method to class Farm for adding and removing animals

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class Farm {
 
+    ArrayList<Animal> barn = new ArrayList<>();
 
+    public void addAnimal(Animal animal){
+        barn.add(animal);
+    }
 
+    public void removeAnimal(int index){
+        System.out.println(barn.toString());
+        barn.remove(index);
+    }
 }
